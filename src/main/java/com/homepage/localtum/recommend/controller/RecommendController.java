040b26345fde941508ac774ef59bc5ae6a88d738
yujin <102959835+0hyujin@users.noreply.github.com>
2024-07-28 18:52:13 +0900

    ♻️@EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/homepage/localtum/recommend/controller/RecommendController.java
+++ src/main/java/com/homepage/localtum/recommend/controller/RecommendController.java
@@ -15,7 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/localTum")
+@RequestMapping("/localtum")
 @RequiredArgsConstructor
 public class RecommendController {
 
