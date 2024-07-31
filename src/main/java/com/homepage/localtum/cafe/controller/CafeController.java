2b1721cfd1d5bfb49df1aaab1d625c471473a660
yujin <102959835+0hyujin@users.noreply.github.com>
2024-07-31 23:20:21 +0900

    ✨스탬프 조회

--- src/main/java/com/homepage/localtum/cafe/controller/CafeController.java
+++ src/main/java/com/homepage/localtum/cafe/controller/CafeController.java
@@ -4,6 +4,7 @@ import com.homepage.localtum.cafe.dto.CreateCafeDto;
 import com.homepage.localtum.cafe.repository.CafeRepository;
 import com.homepage.localtum.cafe.service.CafeService;
 import com.homepage.localtum.domain.Cafe;
+import com.homepage.localtum.order.service.OrderService;
 import com.homepage.localtum.util.Member.AuthenticationMemberUtils;
 import com.homepage.localtum.util.response.CustomApiResponse;
 import jakarta.transaction.Transactional;
@@ -35,9 +36,16 @@ public class CafeController {
 //        return result;
 //    }
 
-    @GetMapping("/search/like/list")
-    public List<Cafe> getAllCafes() {
-        return cafeService.getAllCafes();
-    }
+//    @GetMapping("/search/like/list")
+//    public List<Cafe> getAllCafes() {
+//        return cafeService.getAllCafes();
+//    }
 
+    // 스탬프 조회
+    @GetMapping("/cafe_details/{cafe_name}")
+    public ResponseEntity<CustomApiResponse<?>> getStamps(@PathVariable("cafe_name") String cafename) {
+        String currentMemberId = memberUtils.getCurrentMemberId();
+        ResponseEntity<CustomApiResponse<?>> result = cafeService.getStamps(currentMemberId, cafename);
+        return result;
+    }
 }
