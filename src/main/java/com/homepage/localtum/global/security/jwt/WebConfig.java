69d531a7a225d4dd7c70c5313206accfcd10bdda
ijunsoo <159103870+ijunsoo@users.noreply.github.com>
2024-08-02 04:42:22 +0900

    :sparkles: cors 허용 설정

---
+++ src/main/java/com/homepage/localtum/global/security/jwt/WebConfig.java
@@ -0,0 +1,17 @@
+package com.homepage.localtum.global.security.jwt;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+                .allowedOrigins("http://15.165.139.216.nip.io") // 허용할 출처. 모든 도메인 허용하려면 "*"
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true); // 인증 정보를 포함할 수 있도록 허용
+    }
+}
